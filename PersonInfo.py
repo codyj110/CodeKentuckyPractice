5c23cedbdb43f420b5aeb690dfdf1bc1a6ed97cd
Jesus Calvo <jesuslopezcalvo@hotmail.com>
2022-05-14 11:20:39 -0400

    Added - Are you enjoying KentuckyCode

--- PersonInfo.py
+++ PersonInfo.py
@@ -2,7 +2,11 @@
 name = input("enter your name: ")
 #function input will write the string give and put the user response into variable age
 age = input("enter your age: ")
-
+x = KentuckyCode = input("are you enjoying the course so far? (enter yes/no) ")
+if x == "yes":
+    print("Awesome")
+else:
+    print("Sorry to hear that")
 # two methods are used here print to print the string to the user and .format puts the variables name and age
 # into the two brackets {}
 print("{} {}".format(name, age))
